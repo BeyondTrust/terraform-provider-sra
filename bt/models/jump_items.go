c935a1ee8489d03c633bc0fbc306fd448f8b66f1
Matthew Hillman <mhillman@beyondtrust.com>
2023-02-27 20:11:03 -0600

    Composed models didn't work

--- bt/models/jump_items.go
+++ bt/models/jump_items.go
@@ -6,16 +6,16 @@ import (
 
 type ShellJumpModel struct {
 	ID              types.String `tfsdk:"id"`
-	Name            types.String `tfsdk:"name" filter:"name"`
-	JumpointID      types.Int64  `tfsdk:"jumpoint_id" filter:"jumpoint_id"`
-	Hostname        types.String `tfsdk:"hostname" filter:"hostname"`
+	Name            types.String `tfsdk:"name"`
+	JumpointID      types.Int64  `tfsdk:"jumpoint_id"`
+	Hostname        types.String `tfsdk:"hostname"`
 	Protocol        types.String `tfsdk:"protocol"`
 	Port            types.Int64  `tfsdk:"port"`
-	JumpGroupID     types.Int64  `tfsdk:"jump_group_id" filter:"jump_group_id"`
-	JumpGroupType   types.String `tfsdk:"jump_group_type" filter:"jump_group_type"`
+	JumpGroupID     types.Int64  `tfsdk:"jump_group_id"`
+	JumpGroupType   types.String `tfsdk:"jump_group_type"`
 	Terminal        types.String `tfsdk:"terminal"`
 	KeepAlive       types.Int64  `tfsdk:"keep_alive"`
-	Tag             types.String `tfsdk:"tag" filter:"tag"`
+	Tag             types.String `tfsdk:"tag"`
 	Comments        types.String `tfsdk:"comments"`
 	JumpPolicyID    types.Int64  `tfsdk:"jump_policy_id"`
 	Username        types.String `tfsdk:"username"`
