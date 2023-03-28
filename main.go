cf0ae681c10fe61682e88a9af9b7bb589cfe0878
Matthew Hillman <mhillman@beyondtrust.com>
2023-03-28 15:27:24 -0500

    Update docs with new module name

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Provider documentation generation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name bt
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name sra
 
 func main() {
 	providerserver.Serve(context.Background(), bt.New, providerserver.ServeOpts{
