2d2d6b53aa6388423fd4f0b93e4965905d6a2c8b
Matthew Hillman <mhillman@beyondtrust.com>
2023-03-28 20:48:42 -0500

    Cleanup

--- bt/rs/remote_vnc.go
+++ bt/rs/remote_vnc.go
@@ -22,29 +22,14 @@ var (
 	// _ resource.ResourceWithModifyPlan  = &remoteVNCResource{}
 )
 
-// Factory function to generate a new resource type. This must be in the
-// main list of resource functions in api_resource.go
 func newRemoteVNCResource() resource.Resource {
 	return &remoteVNCResource{}
 }
 
-// The main type for the resource. By convention this should be in the form
-//
-//	<resourceName>Resource
-//
-// This type name of the API model will be used to generate the public name
-// for this resource that is used in the *.tf files. The
-// public name will be converted like:
-//
-//	ResourceName -> sra_resource_name
 type remoteVNCResource struct {
-	// Compose with the main apiResource struct to get all the boilerplate
-	// implementations. Types are: this resource, api model, terraform model
 	apiResource[api.RemoteVNC, models.RemoteVNCModel]
 }
 
-// We must define the schema for each resource individually. Anything that can be supplied by the API response
-// needs to be marked as "Computed", even if we translate from "null" on a POST to an empty string
 func (r *remoteVNCResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Description: "Manages a Remote VNC Jump Item.\n\nFor descriptions of individual fields, please see the Configuration API documentation on your SRA Appliance",
