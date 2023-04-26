c0a2c24783b4af56c61e1f9440ca7799684d6000
Matthew Hillman <mhillman@beyondtrust.com>
2023-04-26 16:23:44 -0500

    Start cleaning up test tf files

--- test-tf-files/jump_items/main.tf
+++ test-tf-files/jump_items/main.tf
@@ -23,56 +23,9 @@ locals {
 }
 
 // Configuration
-
-resource "sra_protocol_tunnel_jump" "example" {
-  name               = "Example TCP Tunnel"
-  hostname           = "example.host"
-  jumpoint_id        = local.jumpoint_id
-  jump_group_id      = local.jump_group_id
-  tunnel_definitions = "22;24;80;8080"
-}
-resource "sra_protocol_tunnel_jump" "mssql" {
-  name          = "Example MSSQL Tunnel"
-  hostname      = "example.database"
-  jumpoint_id   = local.jumpoint_id
-  jump_group_id = local.jump_group_id
-  tunnel_type   = "mssql"
-  username      = "db_user"
-}
-resource "sra_remote_rdp" "item" {
-  name          = "fun_rdp"
-  hostname      = "10.10.10.10"
-  jumpoint_id   = local.jumpoint_id
-  jump_group_id = local.jump_group_id
-}
-resource "sra_remote_vnc" "item" {
-  name          = local.name
-  hostname      = local.hostname
-  jumpoint_id   = local.jumpoint_id
-  jump_group_id = local.jump_group_id
-}
-resource "sra_shell_jump" "item" {
-  name          = local.name
-  hostname      = local.hostname
-  jumpoint_id   = local.jumpoint_id
-  jump_group_id = local.jump_group_id
-}
 resource "sra_web_jump" "item" {
   name          = "Example Web Jump"
   url           = "https://example.host/login"
   jumpoint_id   = local.jumpoint_id
   jump_group_id = local.jump_group_id
-}
-
-// Output
-
-output "items" {
-  value = {
-    ProtocolTunnel = resource.sra_protocol_tunnel_jump.example
-    MSSQLTunnel    = resource.sra_protocol_tunnel_jump.mssql
-    RemoteRDP      = resource.sra_remote_rdp.item
-    RemoteVNC      = resource.sra_remote_vnc.item
-    ShellJump      = resource.sra_shell_jump.item
-    WebJump        = resource.sra_web_jump.item
-  }
 }
