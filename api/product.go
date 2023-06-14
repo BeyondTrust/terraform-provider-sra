e24d170cca9867f7981250f60cab26582dfc84e2
Matthew Hillman <mhillman@beyondtrust.com>
2023-06-14 13:42:35 -0500

    Add product checking functions

--- api/product.go
+++ api/product.go
@@ -1,10 +1,17 @@
 package api
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
 type CtxKey string
 
 const ProductKey CtxKey = "product"
-const ProductRS = "rs"
-const ProductPRA = "pra"
+const ProductRS = "RS"
+const ProductPRA = "PRA"
 
 var (
 	product = ProductPRA
@@ -22,4 +29,33 @@ func IsRS() bool {
 }
 func IsPRA() bool {
 	return product == ProductPRA
+}
+func ProductName() string {
+	return product
+}
+
+type RestrictsProducts interface {
+	AllowRS() bool
+	AllowPRA() bool
+}
+
+func IsProductAllowed(ctx context.Context, i interface{}) bool {
+	s, ok := i.(RestrictsProducts)
+
+	if !ok {
+		tflog.Trace(ctx, fmt.Sprintf("🌈 Not OK [%+v]\n", i))
+		// Doesn't restrict products, so everything is allowed
+		return true
+	}
+
+	if !s.AllowRS() && IsRS() {
+		tflog.Trace(ctx, fmt.Sprintf("🌈 Not RS [%+v]\n", s))
+		return false
+	}
+	if !s.AllowPRA() && IsPRA() {
+		tflog.Trace(ctx, fmt.Sprintf("🌈 Not PRA [%+v]\n", s))
+		return false
+	}
+	tflog.Trace(ctx, fmt.Sprintf("🌈 Yes, allowed [%+v]\n", s))
+	return true
 }
--- api/product_test.go
+++ api/product_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +17,68 @@ func TestProductSetting(t *testing.T) {
 	SetProductIsRS(false)
 	assert.True(t, IsPRA())
 	assert.False(t, IsRS())
+}
+
+func TestProductName(t *testing.T) {
+	// t.Parallel()
+
+	SetProductIsRS(true)
+	assert.Equal(t, ProductRS, ProductName())
+
+	SetProductIsRS(false)
+	assert.Equal(t, ProductPRA, ProductName())
+}
+
+type noInterface struct{}
+
+type allProducts struct{}
+
+func (p allProducts) AllowPRA() bool {
+	return true
+}
+
+func (p allProducts) AllowRS() bool {
+	return true
+}
+
+type productFeature struct {
+	product string
+}
+
+func (p productFeature) AllowPRA() bool {
+	return p.product == ProductPRA
+}
+
+func (p productFeature) AllowRS() bool {
+	return p.product == ProductRS
+}
+
+func TestProductRestriction(t *testing.T) {
+	p := &productFeature{ProductPRA}
+	assert.True(t, p.AllowPRA())
+	assert.False(t, p.AllowRS())
+
+	r := &productFeature{ProductRS}
+	assert.False(t, r.AllowPRA())
+	assert.True(t, r.AllowRS())
+
+	n := &noInterface{}
+
+	a := &allProducts{}
+	assert.True(t, a.AllowPRA())
+	assert.True(t, a.AllowRS())
+
+	ctx := context.Background()
+
+	SetProductIsRS(true)
+	assert.False(t, IsProductAllowed(ctx, p))
+	assert.True(t, IsProductAllowed(ctx, r))
+	assert.True(t, IsProductAllowed(ctx, n))
+	assert.True(t, IsProductAllowed(ctx, a))
+
+	SetProductIsRS(false)
+	assert.True(t, IsProductAllowed(ctx, p))
+	assert.False(t, IsProductAllowed(ctx, r))
+	assert.True(t, IsProductAllowed(ctx, n))
+	assert.True(t, IsProductAllowed(ctx, a))
 }
