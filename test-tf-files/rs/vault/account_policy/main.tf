36e7cc99c6859176be08444a14293cd9640ee565
Matthew Hillman <mhillman@beyondtrust.com>
2023-06-28 10:16:38 -0500

    Fix RS test file

--- test-tf-files/rs/vault/account_policy/main.tf
+++ test-tf-files/rs/vault/account_policy/main.tf
@@ -11,15 +11,16 @@ terraform {
 
 resource "sra_vault_account_policy" "new_account_policy" {
   name                        = "${var.name} ${var.random_bits}"
-  description                 = var.random_bits
+  code_name                   = var.random_bits
   auto_rotate_credentials     = true
   allow_simultaneous_checkout = true
   scheduled_password_rotation = true
+  maximum_password_age        = 10
 }
 
 resource "sra_vault_account_policy" "new_account_policy_false" {
   name                        = "${var.name} ${var.random_bits} False"
-  description                 = var.random_bits
+  code_name                   = "${var.random_bits}_false"
   auto_rotate_credentials     = false
   allow_simultaneous_checkout = false
   scheduled_password_rotation = false
@@ -27,12 +28,12 @@ resource "sra_vault_account_policy" "new_account_policy_false" {
 
 resource "sra_vault_account_policy" "new_account_policy_mixed" {
   name                        = "${var.name} ${var.random_bits} Mixed"
-  description                 = var.random_bits
+  code_name                   = "${var.random_bits}_mixed"
   auto_rotate_credentials     = false
   allow_simultaneous_checkout = true
   scheduled_password_rotation = false
 }
 
 data "sra_vault_account_policy_list" "ap" {
-  name = "${var.name} ${var.random_bits}"
+  code_name = var.random_bits
 }
